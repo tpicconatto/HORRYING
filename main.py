8574c629920d7dd983c3d488142669694dd9f499
tpicconatto <tpicconatto@potomacschool.org>
2022-11-03 07:11:46 -0400

    Display black screen

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 # import the pygame module, so you can use it
 import pygame
+# initialize the pygame module
+pygame.init()
+vec = pygame.math.Vector2  # 2 for two dimensional
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+HEIGHT = 500
+WIDTH = 500
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("HORRIFYING")
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 
-# define a main function
-def main():
-    # initialize the pygame module
-    pygame.init()
-    # load and set the logo
-    logo = pygame.image.load("logo32x32.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
-
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240, 180))
-
-    # define a variable to control the main loop
-    running = True
-
-    # main loop
-    while running:
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
-
-
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
-if __name__ == "__main__":
-    # call the main function
-    main()
