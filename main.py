d9d06fda3a0c3a9825b28c9f5a0b12c5d4e7a71d
tpicconatto <tpicconatto@potomacschool.org>
2022-11-03 19:40:56 -0400

    fooling around and finding out

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 # import the pygame module, so you can use it
 import pygame
+from settings import *
+from Blocks import Tile
 # initialize the pygame module
-pygame.init()
-vec = pygame.math.Vector2  # 2 for two dimensional
-ACC = 0.5
-FRIC = -0.12
-FPS = 60
 
-HEIGHT = 500
-WIDTH = 500
+
+#for row_index, row in enumerate(layout):
+    #for col_index, cell in enumerate (row):
+        #x = col_index * tile_size
+        #y = row_index * tile_size
+        #if cell == 'X':
+            #tile = Tile((x,y),tile_size)
+            #self.tiles.add(tile)
+#class Ghost(pygame.sprite.Sprite):
+    #def __init__(self,width,height,posX,posY,color):
+        #super.__init__()
+        #self.image = pygame.Surface([width,height])
+        #self.image.fill(color)
+        #self.rect = self.image.get_rect()
+
+
+pygame.init()
+clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("HORRIFYING")
-pygame.display.flip()
+test_tile = pygame.sprite.Group(Tile((100,100),200))
+test_tile.draw(screen)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill('black')
+    test_tile.draw(screen)
+    pygame.display.update()
+    clock.tick(60)
 
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+level1_map = {
+'                  ',
+'                  ',
+'    x     x   x   '    
+'   xx             ',
+' xxxx  xx  x  x   ',
+'xxxxx  xxx xx  xxx'
+}
+tile_size=64
+HEIGHT = len(level1_map) * tile_size
+WIDTH = 1200
