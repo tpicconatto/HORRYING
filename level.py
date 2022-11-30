205a6868546d84584bf1fc52c3972fb12d120571
Benjamin-Bartlett <bbartlett@potomacschool.org>
2022-11-30 09:13:09 -0500

    Added levels

--- level.py
+++ level.py
@@ -16,23 +16,84 @@ class Level():
 
         if self.number == 1:
             self.level1()
+        elif self.number == 2:
+            self.level2()
+        elif self.number == 3:
+            self.level3()
+        elif self.number == 4:
+            self.level4()
+        elif self.number == 5:
+            self.level5()
+
 
     def level1(self):
         planet1 = Planet(self.screen, pygame.math.Vector2(600, 400), 'Assets/PurplePlanet.png')
         planet1.display()
         self.bodiesList.append(planet1)
+
+    def level2(self):
+        planet1 = Planet(self.screen, pygame.math.Vector2(300, 500), 'Assets/PurplePlanet.png')
+        planet1.display()
+        self.bodiesList.append(planet1)
+
+        planet2 = Planet(self.screen, pygame.math.Vector2(900, 300), 'Assets/DesertPlanet.png')
+        planet2.display()
+        self.bodiesList.append(planet2)
+
+
+    def level3(self):
+        planet1 = Planet(self.screen, pygame.math.Vector2(500, 400), 'Assets/PurplePlanet.png')
+        planet1.display()
+        self.bodiesList.append(planet1)
+
+        planet2 = Planet(self.screen, pygame.math.Vector2(700, 600), 'Assets/DesertPlanet.png')
+        planet2.display()
+        self.bodiesList.append(planet2)
+
+        planet3 = Planet(self.screen, pygame.math.Vector2(800, 250), 'Assets/DesertPlanet.png')
+        planet3.display()
+        self.bodiesList.append(planet3)
+
+    def level4(self):
+        planet1 = Planet(self.screen, pygame.math.Vector2(600, 400), 'Assets/PurplePlanet.png')
+        planet1.display()
+        self.bodiesList.append(planet1)
+
+        planet2 = Planet(self.screen, pygame.math.Vector2(750, 300), 'Assets/DesertPlanet.png')
+        planet2.display()
+        self.bodiesList.append(planet2)
+
+        planet3 = Planet(self.screen, pygame.math.Vector2(800, 600), 'Assets/DesertPlanet.png')
+        planet3.display()
+        self.bodiesList.append(planet3)
+
+        planet4 = Planet(self.screen, pygame.math.Vector2(200, 300), 'Assets/DesertPlanet.png')
+        planet4.display()
+        self.bodiesList.append(planet4)
+
+
+
+    def level5(self):
+        planet1 = Planet(self.screen, pygame.math.Vector2(600, 400), 'Assets/PurplePlanet.png')
+        planet1.display()
+        self.bodiesList.append(planet1)
+
         planet2 = Planet(self.screen, pygame.math.Vector2(100, 100), 'Assets/PurplePlanet.png')
         planet2.display()
         self.bodiesList.append(planet2)
-        planet5 = Planet(self.screen, pygame.math.Vector2(950, 150), 'Assets/DesertPlanet.png')
+
+        planet3 = Planet(self.screen, pygame.math.Vector2(950, 150), 'Assets/DesertPlanet.png')
+        planet3.display()
+        self.bodiesList.append(planet3)
+
+        planet4 = Planet(self.screen, pygame.math.Vector2(550, 650), 'Assets/DesertPlanet.png')
+        planet4.display()
+        self.bodiesList.append(planet4)
+
+        planet5 = Planet(self.screen, pygame.math.Vector2(400, 250), 'Assets/DesertPlanet.png')
         planet5.display()
         self.bodiesList.append(planet5)
-        planet6 = Planet(self.screen, pygame.math.Vector2(550, 650), 'Assets/DesertPlanet.png')
-        planet6.display()
-        self.bodiesList.append(planet6)
-        planet7 = Planet(self.screen, pygame.math.Vector2(400, 250), 'Assets/DesertPlanet.png')
-        planet7.display()
-        self.bodiesList.append(planet7)
+
         # planet4 = Planet(self.screen, 1000, 500)
         # planet4.display()
         # earth = Earth(self.screen,1100,700)
